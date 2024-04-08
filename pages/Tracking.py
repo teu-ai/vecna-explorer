57c6db71368ffcb526890d1d688cc8e9b6e953ec
Alvaro Serrano <marran51@gmail.com>
2024-04-08 16:44:43 +0000

    Revert "add envio 50"
    This reverts commit 7ae9f1c8e855b7a26bcaea5d92fc5e0af70f453b.

--- pages/Tracking.py
+++ pages/Tracking.py
@@ -33,8 +33,7 @@ ENVIOS = [
     'Envío 18',
     'Envío 19',
     'Envío 47',
-    'Envío CONT',
-    'Envío 50'
+    'Envío CONT'
 ]
 PROBLEMS_TO_IGNORE = [
     "W. Sin BL",
@@ -94,7 +93,6 @@ def plot_errors_per_envio(data):
         datetime(2023,8,4),
         datetime(2023,11,20),
         datetime(2024,1,5),
-        datetime(2024,4,5),
     ]
     plot = alt.Chart(source).mark_point().encode(
         x=alt.X("Fecha",title="Envío de datos"),
@@ -129,7 +127,7 @@ with col1_a:
     data_quality_wide = data_quality_wide.loc[lambda x: x["Envío de datos"].apply(lambda y: y in ENVIOS)]
 
 # Create selectbox with Envío de datos
-envios_de_datos_default = ['Envío 50']
+envios_de_datos_default = []
 with col2_a:
     # envios_de_datos = data_quality_wide[["Envío de datos"]].drop_duplicates()["Envío de datos"].dropna().tolist()    
     envios_de_datos = ENVIOS
