e7d99b08beb65a691dafbdc5376612e9780dbc12
Alvaro Serrano <marran51@gmail.com>
2024-04-08 16:44:46 +0000

    Revert "prueba"
    This reverts commit f1a8ac12c62a4c212a916441955d11799018c2e6.

--- pages/Tracking.py
+++ pages/Tracking.py
@@ -33,7 +33,8 @@ ENVIOS = [
     'Envío 18',
     'Envío 19',
     'Envío 47',
-    'Envío CONT'
+    'Envío CONT',
+    'Envío 50'
 ]
 PROBLEMS_TO_IGNORE = [
     "W. Sin BL",
@@ -93,6 +94,7 @@ def plot_errors_per_envio(data):
         datetime(2023,8,4),
         datetime(2023,11,20),
         datetime(2024,1,5),
+        datetime(2024,4,5),
     ]
     plot = alt.Chart(source).mark_point().encode(
         x=alt.X("Fecha",title="Envío de datos"),
@@ -127,7 +129,7 @@ with col1_a:
     data_quality_wide = data_quality_wide.loc[lambda x: x["Envío de datos"].apply(lambda y: y in ENVIOS)]
 
 # Create selectbox with Envío de datos
-envios_de_datos_default = []
+envios_de_datos_default = ["Envío 50"]
 with col2_a:
     # envios_de_datos = data_quality_wide[["Envío de datos"]].drop_duplicates()["Envío de datos"].dropna().tolist()    
     envios_de_datos = ENVIOS
