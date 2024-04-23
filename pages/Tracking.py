f19a8e728e8b9a26f8e31d8a8df6f086c66f0237
Alvaro Serrano <marran51@gmail.com>
2024-04-23 21:00:36 +0000

    envio 52

--- pages/Tracking.py
+++ pages/Tracking.py
@@ -35,7 +35,8 @@ ENVIOS = [
     'Envío 47',
     'Envío CONT',
     'Envío 50',
-    'Envío 51'
+    'Envío 51',
+    'Envío 52',
 ]
 PROBLEMS_TO_IGNORE = [
     "W. Sin BL",
@@ -97,6 +98,7 @@ def plot_errors_per_envio(data):
         datetime(2024,1,5),
         datetime(2024,4,5),
         datetime(2024,4,12),
+        datetime(2024,4,23),
     ]
     plot = alt.Chart(source).mark_point().encode(
         x=alt.X("Fecha",title="Envío de datos"),
@@ -132,7 +134,7 @@ with col1_a:
 
 
 # Create selectbox with Envío de datos
-envios_de_datos_default = [f"Envío {i}" for i in range(50, 52)]
+envios_de_datos_default = [f"Envío {i}" for i in range(50, 53)]
 with col2_a:
     # envios_de_datos = data_quality_wide[["Envío de datos"]].drop_duplicates()["Envío de datos"].dropna().tolist()    
     envios_de_datos = ENVIOS
