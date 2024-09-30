47cdf2c79a2d60ea14b98e6801ac29aeb3099a91
Pablo Sateler <p.sateler93@gmail.com>
2024-09-30 16:51:55 -0300

    add 9 er

--- pages/Tracking.py
+++ pages/Tracking.py
@@ -50,6 +50,7 @@ ENVIOS = [
     'Envío 6_ER',
     'Envío 7_ER',
     'Envío 8_ER',
+    'Envío 9_ER',
 ]
 PROBLEMS_TO_IGNORE = [
     "W. Sin BL",
@@ -125,6 +126,7 @@ def plot_errors_per_envio(data):
         datetime(2024,8,30),
         datetime(2024,9,6),
         datetime(2024,9,13),
+        datetime(2024,9,30),
     ]
     plot = alt.Chart(source).mark_point().encode(
         x=alt.X("Fecha",title="Envío de datos"),
@@ -160,7 +162,7 @@ with col1_a:
 
 
 # Create selectbox with Envío de datos
-envios_de_datos_default = ['Envío 1_ER', 'Envío 2_ER', 'Envío 3_ER', 'Envío 4_ER', 'Envío 5_ER', 'Envío 6_ER', 'Envío 7_ER', 'Envío 8_ER']
+envios_de_datos_default = ['Envío 1_ER', 'Envío 2_ER', 'Envío 3_ER', 'Envío 4_ER', 'Envío 5_ER', 'Envío 6_ER', 'Envío 7_ER', 'Envío 8_ER', 'Envío 9_ER']
 with col2_a:
     # envios_de_datos = data_quality_wide[["Envío de datos"]].drop_duplicates()["Envío de datos"].dropna().tolist()    
     envios_de_datos = ENVIOS
